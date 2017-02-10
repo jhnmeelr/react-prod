109136be01b80fc153ea3507f1717d0ca9c9702b
Ivan Melnyk <jhn.meelr@gmail.com>
2017-02-10 23:11:54 +0200

    5.Refactor React Components to be Stateless Functional Components

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TodoItem = ({ name, isComplete }) => (
+  <li>
+    <input
+      type="checkbox"
+      defaultChecked={isComplete}/>{name}
+  </li>
+);
---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { TodoItem } from './TodoItem';
+
+export const TodoList = ({ todos }) => (
+  <div className="Todo-List">
+    <ul>
+      {todos.map(todo => <TodoItem key={todo.id} {...todo} />)}
+    </ul>
+  </div>
+);
